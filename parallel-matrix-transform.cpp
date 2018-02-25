03ae55eb29f212bf63b061088184b1330cc08881
Brian Knaus <briank.lists@gmail.com>
2018-02-25 14:35:00 -0800

    moved SquareRoot to seperate file

---
+++ parallel-matrix-transform.cpp
@@ -0,0 +1,24 @@
+
+// [[Rcpp::depends(RcppParallel)]]
+#include <RcppParallel.h>
+using namespace RcppParallel;
+
+struct SquareRoot : public Worker
+{
+  // source matrix
+  const RMatrix<double> input;
+  
+  // destination matrix
+  RMatrix<double> output;
+  
+  // initialize with source and destination
+  SquareRoot(const NumericMatrix input, NumericMatrix output) : input(input), output(output) {}
+  
+  // take the square root of the range of elements requested
+  //   void operator()(std::size_t begin, std::size_t end) {
+  //      std::transform(input.begin() + begin, 
+  //                     input.begin() + end, 
+  //                     output.begin() + begin, 
+  //                     ::sqrt);
+  //   }
+};
