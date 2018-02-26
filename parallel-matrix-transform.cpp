8d463cd8d264aad24c0a4141e2344a17cfef5224
Brian Knaus <briank.lists@gmail.com>
2018-02-26 06:54:21 -0800

    update

--- parallel-matrix-transform.cpp
+++ parallel-matrix-transform.cpp
@@ -15,10 +15,10 @@ struct SquareRoot : public Worker
   SquareRoot(const NumericMatrix input, NumericMatrix output) : input(input), output(output) {}
   
   // take the square root of the range of elements requested
-  //   void operator()(std::size_t begin, std::size_t end) {
-  //      std::transform(input.begin() + begin, 
-  //                     input.begin() + end, 
-  //                     output.begin() + begin, 
-  //                     ::sqrt);
-  //   }
+     void operator()(std::size_t begin, std::size_t end) {
+        std::transform(input.begin() + begin, 
+                       input.begin() + end, 
+                       output.begin() + begin, 
+                       ::sqrt);
+     }
 };
